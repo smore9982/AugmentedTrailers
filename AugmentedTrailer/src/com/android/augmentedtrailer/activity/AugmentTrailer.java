98079ce54655a59041566d9ae94aef4b446c3ee0
smore9982 <sameermore412@gmail.com>
2013-10-27 16:54:08 -0400

    Initial Commit

---
+++ AugmentedTrailer/src/com/android/augmentedtrailer/activity/AugmentTrailer.java
@@ -0,0 +1,212 @@
+package com.android.augmentedtrailer.activity;
+
+import com.metaio.R;
+import com.metaio.sdk.ARViewActivity;
+import com.metaio.sdk.MetaioDebug;
+import com.metaio.sdk.jni.IGeometry;
+import com.metaio.sdk.jni.IMetaioSDKCallback;
+import com.metaio.sdk.jni.MetaioSDK;
+import com.metaio.sdk.jni.Rotation;
+import com.metaio.sdk.jni.TrackingValuesVector;
+import com.metaio.sdk.jni.Vector3d;
+import com.metaio.tools.io.AssetsManager;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+public class AugmentTrailer extends ARViewActivity{
+	
+	private IGeometry mTrailerPlaneHalo = null;
+	private IGeometry mTrailerPlaneForza = null;
+	private IGeometry mTrailerPlaneME = null;
+	private IGeometry currentPlane = null;
+	private MetaioSDKCallbackHandler mCallbackHandler;
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		//MetaioSDK.CreateMetaioSDKAndroid(this, R.string.metaioSDKSignature);
+		mCallbackHandler = new MetaioSDKCallbackHandler();
+	}
+		
+	@Override
+	protected void onPostCreate(Bundle savedInstanceState){
+	       super.onPostCreate(savedInstanceState);	       
+	}
+		
+		
+	@Override
+	protected void onStart(){
+		super.onStart();
+	}
+	    
+	@Override
+	protected void onRestart(){
+		super.onRestart();
+	}
+
+	@Override
+	protected void onResume(){
+		super.onResume();
+	}
+		
+	@Override
+	protected void onPause(){
+		super.onPause();
+	}
+		
+	@Override
+	protected void onStop(){
+		super.onStop();
+	}
+		
+	@Override
+	protected void onDestroy(){
+		super.onDestroy();
+	}
+
+	@Override
+	protected int getGUILayout() {	
+		return 0;
+	}
+
+	@Override
+	protected IMetaioSDKCallback getMetaioSDKCallbackHandler() {
+		return this.mCallbackHandler;
+	}
+
+	@Override
+	protected void loadContents() {
+		// Load desired tracking data for planar marker tracking
+		try{
+			final String trackingConfigFile = AssetsManager.getAssetPath("TrackingData_MarkerlessFast.xml");
+			
+			final boolean result = metaioSDK.setTrackingConfiguration(trackingConfigFile); 
+			MetaioDebug.log("Tracking data loaded: " + result); 
+	      		
+			// Loading movie geometry
+			final String haloMoviePath = AssetsManager.getAssetPath("HaloAssets/HaloVideo.3g2");
+			final String forzaMoviePath = AssetsManager.getAssetPath("ForzaAssets/ForzaVideo.3g2");
+			final String massEffectMoviePath = AssetsManager.getAssetPath("MassEffectAssets/MassEffectVideo.3g2");
+			
+			if (haloMoviePath != null)
+			{
+				mTrailerPlaneHalo = metaioSDK.createGeometryFromMovie(haloMoviePath, false);
+				if (mTrailerPlaneHalo != null){
+					mTrailerPlaneHalo.setScale(2.0f);
+					mTrailerPlaneHalo.setRotation(new Rotation(0f, 0f, 0f));
+					mTrailerPlaneHalo.setCoordinateSystemID(1);
+					MetaioDebug.log("Loaded geometry "+haloMoviePath);
+				}
+				else {
+					MetaioDebug.log(Log.ERROR, "Error loading geometry: "+haloMoviePath);
+				}	
+			}
+			
+			if(forzaMoviePath != null){
+				mTrailerPlaneForza = metaioSDK.createGeometryFromMovie(forzaMoviePath, false);
+				if (mTrailerPlaneForza != null){
+					mTrailerPlaneForza.setScale(10.0f);
+					mTrailerPlaneForza.setRotation(new Rotation(0f, 0f, 0f));
+					mTrailerPlaneForza.setCoordinateSystemID(2);
+					MetaioDebug.log("Loaded geometry "+forzaMoviePath);
+				}
+				else {
+					MetaioDebug.log(Log.ERROR, "Error loading geometry: "+forzaMoviePath);
+				}
+			}
+			
+			if(massEffectMoviePath != null){
+				mTrailerPlaneME = metaioSDK.createGeometryFromMovie(massEffectMoviePath, false);
+				if (mTrailerPlaneME != null){
+					mTrailerPlaneME.setScale(5.0f);
+					mTrailerPlaneME.setRotation(new Rotation(0f, 0f, 0f));
+					mTrailerPlaneME.setCoordinateSystemID(3);
+					MetaioDebug.log("Loaded geometry "+forzaMoviePath);
+				}
+				else {
+					MetaioDebug.log(Log.ERROR, "Error loading geometry: "+forzaMoviePath);
+				}
+			}
+						
+			// loading environment maps
+			//final String path = AssetsManager.getAssetPath("Tutorial2/Assets2/env_map.zip");
+			//if (path != null)
+			//{
+			//	boolean loaded = metaioSDK.loadEnvironmentMap(path);
+			//	MetaioDebug.log("Environment map loaded: " + loaded);
+			//}
+			//else
+			//{
+				//MetaioDebug.log(Log.ERROR, "Environment map not found at: "+path);
+			//}
+
+			// Start by displaying metaio man (hide other models)
+			//setActiveModel(0);
+		}
+		catch (Exception e)
+		{
+			e.printStackTrace();
+			MetaioDebug.log(Log.ERROR, "loadContents failed, see stack trace");
+		}
+	}
+
+	@Override
+	protected void onGeometryTouched(IGeometry geometry) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	final private class MetaioSDKCallbackHandler extends IMetaioSDKCallback{
+		@Override
+		public void onSDKReady() 
+		{
+			// show GUI after SDK is ready
+			runOnUiThread(new Runnable() 
+			{
+				@Override
+				public void run() 
+				{					
+				}
+			});
+		}
+		
+		@Override
+		public void onTrackingEvent(TrackingValuesVector trackingValues)
+		{
+			super.onTrackingEvent(trackingValues);
+			String cosName = trackingValues.get(0).getCosName();			
+			String sensor = trackingValues.get(0).getSensor();
+			int cosId = trackingValues.get(0).getCoordinateSystemID();
+			String addValues = trackingValues.get(0).getAdditionalValues();
+			long size = trackingValues.size();
+						
+			Log.i("TRACKING", "SensorName: " + sensor);
+			Log.i("TRACKING", "cosName: " + cosName);
+			Log.i("TRACKING", "COSID: " + cosId);
+			Log.i("TRACKING","AdditonalValues: " + addValues);
+			Log.i("TRACKING", "SIZE: " + size);
+			
+			if(currentPlane !=null){
+				currentPlane.stopMovieTexture();
+			}
+			
+			
+			if(cosName.equalsIgnoreCase("HaloCOS") && mTrailerPlaneHalo !=null){
+				currentPlane = mTrailerPlaneHalo;
+				currentPlane.startMovieTexture();
+			}
+			
+			if(cosName.equalsIgnoreCase("ForzaCOS") && mTrailerPlaneForza !=null){
+				currentPlane = mTrailerPlaneForza;
+				currentPlane.startMovieTexture();
+			}
+			
+			if(cosName.equalsIgnoreCase("MECos") && mTrailerPlaneME !=null){
+				currentPlane = mTrailerPlaneME;
+				currentPlane.startMovieTexture();
+			}			
+		}
+	}
+}
---
+++ AugmentedTrailer/src/com/android/augmentedtrailer/activity/SplashScreenActivity.java
@@ -0,0 +1,124 @@
+package com.android.augmentedtrailer.activity;
+
+
+import java.io.IOException;
+
+import com.android.augmentedtrailer.R;
+import com.metaio.sdk.MetaioDebug;
+import com.metaio.tools.io.AssetsManager;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
+import android.view.View;
+import android.webkit.WebSettings;
+import android.widget.Toast;
+
+public class SplashScreenActivity extends Activity {
+	Handler _handler; 
+	AssetsExtracter mTask;
+	
+	private Runnable delayRunnable = new Runnable(){
+		public void run() {
+			SplashScreenActivity.this.loadNextScreen();
+		}
+	};
+	
+	private void loadNextScreen(){
+		Intent nextScreen = new Intent(this,AugmentTrailer.class);
+		this.startActivity(nextScreen);
+		this.finish();
+	}
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		setContentView(R.layout.activity_splashscreen);
+		super.onCreate(savedInstanceState);
+		mTask = new AssetsExtracter();
+		mTask.execute(0);
+	}
+		
+	@Override
+	protected void onPostCreate(Bundle savedInstanceState){
+	       super.onPostCreate(savedInstanceState);
+	}
+		
+		
+	@Override
+	protected void onStart(){
+		super.onStart();
+	}
+	    
+	@Override
+	protected void onRestart(){
+		super.onRestart();
+	}
+
+	@Override
+	protected void onResume(){
+		super.onResume();
+	}
+		
+	@Override
+	protected void onPause(){
+		super.onPause();
+	}
+		
+	@Override
+	protected void onStop(){
+		super.onStop();
+    	if (this._handler != null){
+    		this._handler.removeCallbacks(this.delayRunnable);
+    	}
+	}
+		
+	@Override
+	protected void onDestroy(){
+		super.onDestroy();
+	}
+	
+	private class AssetsExtracter extends AsyncTask<Integer, Integer, Boolean>
+	{
+
+		@Override
+		protected void onPreExecute() 
+		{			
+		}
+		
+		@Override
+		protected Boolean doInBackground(Integer... params) 
+		{
+			try 
+			{
+				AssetsManager.extractAllAssets(getApplicationContext(), true);
+			} 
+			catch (IOException e) 
+			{
+				MetaioDebug.printStackTrace(Log.ERROR, e);
+				return false;
+			}
+
+			return true;
+		}
+		
+		@Override
+		protected void onPostExecute(Boolean result) 
+		{			
+			
+			if (result)
+			{
+				Toast.makeText(SplashScreenActivity.this, "Metaio Assets Loaded", Toast.LENGTH_SHORT);
+				_handler = new Handler();
+				_handler.postDelayed(delayRunnable, 10);
+			}
+			else
+			{
+				MetaioDebug.log(Log.ERROR, "Error extracting assets, closing the application...");
+				finish();
+			}
+	    }
+	}
+}
